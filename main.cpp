0bb8111f54735f884efb6030bbdcbb3e8268f286
buildingBuild <eneojo.solomon.u@gmail.com>
2025-04-10 20:20:46 -0500

    Completed Contact adding/checks for duplicates and did the base for display all contacts

--- main.cpp
+++ main.cpp
@@ -1,87 +1,190 @@
 #include <iostream>
-#include <fstream>
-#include <map>
-#include <utility>
+#include <fstream> // for file handling
+#include <map>     // um for maps obviously lol
+#include <utility> // for pairs
 using namespace std;
 
 int main()
 {
 
-    long double num1;
-    long double num2;
-    long double result;
-    cin >> num1;
-    cin >> num2;
-    result = num1 - num2;
+    map<string, pair<string, string> /* */> Contact_Info; // some old c++ version support
+    map<string, pair<string, string> /* */>::iterator iter;
 
-    cout << result;
-
-    map<string, pair<string, long long> /* */>
-        Contact_Info;
+    // Load for csv file
 
     int userChoice;
+    char userWord = 'Y';
 
-    cout << "Select a choice\n";
-    cout << "1. Add a contact\n";
-    cout << "2. Search for a contact\n";
-    cout << "3. Delete a contact\n";
-    cin >> userChoice;
-    cin.ignore();
-    // Favorites feature
-    // Search by name prefix
-    // Sort by recent contact
-    // Birthday Reminders
-    // Export to file (txt/csv)
-    // Call/SMS simulation logs
-    // interacting with sql
-    // Gui
-    // check if number exists
-    switch (userChoice)
+    cout << "\n";
+    cout << "WELCOME TO CONTACT BOOK!\n";
+    cout << "Numbers are formatted in the Land of the Free Format RAHHHH\n";
+    do
     {
-    case 1:
-    {
-        char userChoice = 'Y';
-        do
+
+        cout << "\n";
+        cout << "Select an option\n";
+        cout << "1. Add a contact\n";
+        cout << "2. Search for a contact\n";
+        cout << "3. Delete a contact\n";
+        cout << "4. Display all contact\n";
+        cout << "Selecting... ";
+        cin >> userChoice;
+        cin.ignore();
+        // Favorites feature
+        // Search by name prefix
+        // Sort by recent contact
+        // Birthday Reminders
+        // Export to file (txt/csv)
+        // Call/SMS simulation logs
+        // interacting with sql
+        // Gui
+        switch (userChoice)
         {
-            string Name;
-            string emailAdd;
-            long int Number;
-            string countryNumberCode;
+        case 1:
+        { /// ADDING CONTACT
 
-            cout << "Enter Contact Name: ";
-            getline(cin, Name);
+            char userChoice = 'Y';
+            do
+            {
+                string Name;
+                string emailAdd;
+                string Number;
+                string countryNumberCode;
 
-            cout << "Enter Contact Email Address: ";
-            getline(cin, emailAdd);
+                //// Start
+                bool Name_Key_exist = false; //  preparing to check if name which is key for map exists to prevent overwritting
+                do
+                {
+                    cout << "\n";
+                    cout << "Adding Contact\n";
+                    cout << "-----------------\n";
 
-            cout << "Enter Contact Number: ";
-            cin >> Number;
+                    cout << "Input contact name: ";
+                    getline(cin, Name);
+                    if (Contact_Info.find(Name) != Contact_Info.end())
+                    {
+                        Name_Key_exist = true;
+                        cout << "Error! Contact Already exist use different Name\n";
+                        cout << "\n";
+                    }
+                    else
+                    {
+                        Name_Key_exist = false;
+                    }
+                } while (Name_Key_exist == true);
+                bool correctEmailFormat = false; //  preparing to check for right format of email
+                do
+                {
+                    cout << "Provide email address: ";
+                    getline(cin, emailAdd); /// add an email check
+                    if (emailAdd.find("@gmail.com") == string::npos && emailAdd.find("@yahoo.com") == string::npos && emailAdd.find("@outlook.com") == string::npos)
+                    {
+                        correctEmailFormat = false;
+                        cout << "Wrong email format{ Try emailName@gmail.com or emailName@yahoo.com or @outlook.com\n";
+                        cout << "\n";
+                        continue;
+                    }
+                    correctEmailFormat = true;
+                } while (correctEmailFormat == false);
 
-            Contact_Info[Name] = make_pair(emailAdd, Number);
-            cout << "Do you want to continue adding contacts:(Y/N): ";
-            cin >> userChoice;
-            cin.ignore();
-            userChoice = toupper(userChoice);
-        } while (userChoice == 'Y');
-        break;
-    }
+                bool correctNumberFormat = true;
+                do
+                {
+                    cout << "Input contact's Number (###-###-####): ";
+                    cin >> Number;
 
-    case 2:
-    {
-        cout << "Enter Contact Name or Number to search \n";
+                    if (Number[3] != '-' || Number[7] != '-' || Number.size() != 12)
+                    {
+                        correctNumberFormat = false;
+                    }
+                    else
+                    {
+                        for (int i = 0; i < Number.size(); i++)
+                        {
+                            correctNumberFormat = true;
 
-        break;
-    }
+                            if (i == 3 || i == 7)
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                if (!isdigit(Number[i]))
+                                {
+                                    correctNumberFormat = false;
+                                    break;
+                                }
+                            }
+                        }
+                    }
 
-    case 3:
-    {
+                    if (correctNumberFormat == false)
+                    {
+                        cout << "\n";
+                        cout << "Invalid input, format should be -- (###-###-###)\n";
+                        cout << "\n";
+                    }
+                } while (correctNumberFormat == false);
 
-        break;
-    }
-    default:
-        break;
-    }
+                Contact_Info[Name] = make_pair(emailAdd, Number);
+                cout << "Contact added successfully!\n";
+                cout << "\n";
+                cout << "Do you want to continue adding contacts:(Y/N): ";
+                cin >> userChoice;
+                cin.ignore();
+                userChoice = toupper(userChoice);
 
+            } while (userChoice == 'Y');
+            break;
+        }
+
+        case 2:
+        {
+            cout << "\n";
+            cout << "Adding Contact\n";
+            cout << "-----------------\n";
+
+            cout << "Enter Contact Name or Number to search \n";
+
+            break;
+        }
+
+        case 3:
+        {
+            cout << "\n";
+            cout << "Searching for Contact\n";
+            cout << "-----------------\n";
+
+            break;
+        }
+
+        case 4:
+        {
+            cout << "\n";
+            cout << "Displaying All Contacts" << endl;
+            int i = 0;
+            // cout to csv file
+            for (iter = Contact_Info.begin(); iter != Contact_Info.end(); ++iter)
+            {
+                cout << "-------------------------------\n";
+                cout << "Contact: " << i + 1 << "\n";
+                cout << "Name: " << iter->first << "\n";
+                cout << "Email Address: " << iter->second.first << "\n";
+                cout << "Phone Number: " << iter->second.second << "\n";
+                cout << "-------------------------------\n";
+                ++i;
+            }
+            break;
+        }
+        default:
+            break;
+        }
+        cout << "Do you want to continue to Main Menu(Y/N): ";
+        cin >> userWord;
+        userWord = toupper(userWord);
+        cin.ignore();
+
+    } while (userWord == 'Y');
     cout << "Hello we are winning you hear me dude can you hear me\n";
     return 0;
 }
